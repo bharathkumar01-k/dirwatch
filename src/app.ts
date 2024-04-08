50d26211208eca4011d46b256f867647ba293e59
Bharath GitHub <bharathkumar01.k@gmail.com>
2024-04-08 08:33:29 +0530

    added error handlers

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express , {Express, IRouter, Request, Response } from "express";
+import express , { Express, IRouter, NextFunction, Request, Response } from "express";
 import apiRouter from "./router/route";
 import startWorker from "./services/schedule_consumer.service";
 
@@ -17,9 +17,28 @@ app.get('/', (req:Request,res:Response) => {
         message:"The app is running successfully"
     })
 })
-
+process.on('unhandledRejection',(err)=>{
+    console.log(err)
+})
 app.use('/api',router)
 apiRouter(router);
+
+app.use((req:Request,res:Response,next:NextFunction)=>{
+    res.status(404).json({
+        success:false,
+        message:"URL not found"
+    })
+    next()
+})
+
+app.use((err, req:Request,res:Response,next:NextFunction) =>{
+    console.log('err',err)
+    // console.log('error =>',err)
+    return res.status(411).json({
+        success:false,
+        message:"Unprocessable entity"
+    })
+})
 
 app.listen(port , () => {
     console.log(`The app is listening on port - ${port}` )
